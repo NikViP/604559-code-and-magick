ef9efe217730fbcd7327c3d7310cf91400cb09e6
NikViP <p11591@mail.ru>
2018-12-07 11:40:43 +0500

    Промежуточный коммит

--- js/setup.js
+++ js/setup.js
@@ -8,42 +8,22 @@ var surName = ['даМарья', 'Верон', 'Мирабелла', 'Вальц
 var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
 
+var catalogWizards = [];
+
+for (i = 0; i <= 3; i++) {
+  var currentWizard = {};
+  currentWizard.name = 'Иван' + ' ' + 'Верон';
+  currentWizard.coatColor = 'rgb(241, 43, 107)';
+  currentWizard.eyesColor = 'yellow';
+  catalogWizards[i] = currentWizard;
+}
+
 document.querySelector('.setup-similar').classList.remove('hidden');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
-
-var createWizardNames = function () {
-  var wizardNames = firstName[i] + surName[i];
-};
-
-var createWizards = ['name', 'coatColor', 'eyesColor'
-
-]
-
-
-/*var CREATE_WIZARD_NAMES = ['Дамблдор', 'Волдеморт', 'Доктор Стрендж', 'Гарри Поттер'];
-
-var wizards = [
-  {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)'
-  },
-  {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
-  },
-  {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
-  },
-  {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
-  }
-];*/
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
@@ -58,6 +38,7 @@ var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
+
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
